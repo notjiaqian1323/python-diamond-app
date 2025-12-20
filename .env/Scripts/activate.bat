f59255f0cbd2f7afa9943019a40520ba81e69ff8
Shong-628 <shuhonglol567@gmail.com>
2025-12-20 18:22:36 +0800

    Revert "Testing (shong)"
    This reverts commit e870118c370a7161c362c55f48604885749f417c.

--- .env/Scripts/activate.bat
+++ .env/Scripts/activate.bat
@@ -8,15 +8,15 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set "VIRTUAL_ENV=C:\Users\shuho\Downloads\Y3Sem1\BMDS2003 DATA SCIENCE\Assignment\Prototype\python-diamond-app\.env"
+set VIRTUAL_ENV=C:\Users\q12w1\OneDrive\Desktop\Diamond price app\.env
 
 if not defined PROMPT set PROMPT=$P$G
 
 if defined _OLD_VIRTUAL_PROMPT set PROMPT=%_OLD_VIRTUAL_PROMPT%
 if defined _OLD_VIRTUAL_PYTHONHOME set PYTHONHOME=%_OLD_VIRTUAL_PYTHONHOME%
 
-set "_OLD_VIRTUAL_PROMPT=%PROMPT%"
-set "PROMPT=(.env) %PROMPT%"
+set _OLD_VIRTUAL_PROMPT=%PROMPT%
+set PROMPT=(.env) %PROMPT%
 
 if defined PYTHONHOME set _OLD_VIRTUAL_PYTHONHOME=%PYTHONHOME%
 set PYTHONHOME=
@@ -24,8 +24,7 @@ set PYTHONHOME=
 if defined _OLD_VIRTUAL_PATH set PATH=%_OLD_VIRTUAL_PATH%
 if not defined _OLD_VIRTUAL_PATH set _OLD_VIRTUAL_PATH=%PATH%
 
-set "PATH=%VIRTUAL_ENV%\Scripts;%PATH%"
-set "VIRTUAL_ENV_PROMPT=.env"
+set PATH=%VIRTUAL_ENV%\Scripts;%PATH%
 
 :END
 if defined _OLD_CODEPAGE (
